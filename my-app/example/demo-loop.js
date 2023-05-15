a1dbf215ea93b136505124e8928d3cefbafafde1
sontv8 <sontv8@fpt.edu.vn>
2023-05-15 09:04:49 +0700

    Day03 - vong lap

---
+++ my-app/example/demo-loop.js
@@ -0,0 +1,17 @@
+const users = ["datlt", "thienth", "sontv"];
+// for (let i in users) {
+//     console.log(users[i]);
+// }
+
+// for (let item of users) {
+//     console.log(item);
+// }
+/*
+    v1: item - datlt
+    v2: item - thienth
+    v3: item - sontv
+*/
+
+users.forEach((index, item) => {
+    console.log(index);
+})
