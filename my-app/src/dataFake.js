a80ddfd301658d41154c452f2038517c9f17f09a
sontv8 <sontv8@fpt.edu.vn>
2023-05-17 08:46:38 +0700

    Day04 - data

---
+++ my-app/src/dataFake.js
@@ -0,0 +1,5 @@
+export const postList = [
+    { id: 1, title: "Bai viet 1", content: "Noi dung bai viet 1" },
+    { id: 2, title: "Bai viet 2", content: "Noi dung bai viet 2" },
+    { id: 3, title: "Bai viet 3", content: "Noi dung bai viet 3" }
+]
