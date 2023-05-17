4e51b4b4a6e958ed1e47f71680a29b493564b3ac
sontv8 <sontv8@fpt.edu.vn>
2023-05-17 07:58:01 +0700

    Day04 - map,find, filter

--- my-app/example/demo-loop.js
+++ my-app/example/demo-loop.js
@@ -1,4 +1,4 @@
-const users = ["datlt", "thienth", "sontv"];
+// const users = ["datlt", "thienth", "sontv"];
 // for (let i in users) {
 //     console.log(users[i]);
 // }
@@ -12,6 +12,36 @@ const users = ["datlt", "thienth", "sontv"];
     v3: item - sontv
 */
 
-users.forEach((index, item) => {
-    console.log(index);
+// users.forEach((index, item) => {
+//     console.log(index);
+// })
+
+const products = [
+    { id: 1, name: "Product 1", price: 100 },
+    { id: 2, name: "Product 2", price: 200 },
+    { id: 3, name: "Product 3", price: 300 }
+]
+// map
+// const result = products.map(function (item) {
+//     // console.log(item);
+//     return `
+//         <div>${item.name}</div>
+//         <div>${item.price}</div>
+//     `
+// })
+// console.log(result);
+// const app = document.querySelector("#app")
+// app.innerHTML = result.join("");
+
+// find
+// const result = products.find(function (item) {
+//     // console.log(item.id);
+//     return item.id == 3
+// })
+// console.log(result);
+
+// filter
+const result = products.filter(function (item) {
+    return item.id != 1
 })
+console.log(result);
