455c75be5b2665e0f3ca0852abeb4ab73459d55c
sontv8 <sontv8@fpt.edu.vn>
2023-05-26 08:17:23 +0700

    Day 06 - not found page

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -1,16 +1,25 @@
 import AboutPage from "./pages/About"
 import HomePage from "./pages/Home"
 import Navigo from 'navigo'
+import NotFoundPage from "./pages/NotFoundPage"
 
 const app = document.querySelector("#app") //tìm phần tử html có id là app
 
 const router = new Navigo('/') //khởi tạo router từ đối tượng Navigo
 
+const render = (content, container) => {
+    return container.innerHTML = content()
+}
+
 router.on('/', () => { //sử dụng phương thức on để khai báo đường dẫn đến từng page
-    return app.innerHTML = HomePage()
+    return render(HomePage, app)
 })
 router.on('/about', () => {
-    return app.innerHTML = AboutPage()
+    return render(AboutPage, app)
+})
+
+router.notFound(() => {
+    return render(NotFoundPage, app)
 })
 
 router.resolve() //sử dụng phương thức resolve để thực thi 
