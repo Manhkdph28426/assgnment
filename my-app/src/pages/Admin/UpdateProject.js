c5038936e80c9fe45027d686547121c1900633bd
sontv8 <sontv8@fpt.edu.vn>
2023-06-07 09:06:52 +0700

    Day 12: update project

--- my-app/src/pages/Admin/UpdateProject.js
+++ my-app/src/pages/Admin/UpdateProject.js
@@ -2,28 +2,36 @@ import { router, useEffect, useState } from "../../lib"
 
 const UpdateProjectPage = ({ id }) => {
     // console.log(id);
-    const [project, setProject] = useState({})
+    const [project, setProject] = useState({}) //khai báo biến project để lưu trữ data
+
+    // call api để lấy dữ liệu và gán vào biến project
     useEffect(() => {
         fetch(`http://localhost:3000/projectList/${id}`)
             .then(response => response.json())
             .then(data => setProject(data))
     }, [])
+
+    // thêm mới data
     useEffect(() => {
         const updateForm = document.querySelector("#update-form")
+        // tìm tới form update và lắng nghe sự kiện submit
         updateForm.addEventListener("submit", (event) => {
-            event.preventDefault()
+            event.preventDefault() //chặn sự kiện reload trang
+            // lấy dữ liệu từ form
             const newData = {
                 "title": document.querySelector("#title").value,
                 "content": document.querySelector("#content").value
             }
+            // gọi api để update data
             fetch(`http://localhost:3000/projectList/${id}`, {
-                method: "PUT",
+                method: "PUT", //phương thức PUT để cập nhật data
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json' //định dạng dữ liệu gửi đi
                 },
-                body: JSON.stringify(newData)
+                body: JSON.stringify(newData) //chuyển đổi dữ liệu sang định dạng json
             }).then(() => {
                 router.navigate("/admin/project")
+                //sau khi update thành công thì chuyển hướng về trang quản lý
             })
         })
     })
